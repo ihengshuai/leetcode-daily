97f34911a21cb264a4e23a34ca7fabfe6b1c09c1
Peachick <Peachick@aliyun.com>
2022-04-04 18:14:23 +0800

    update: 合并两个有序链表(21)

---
+++ src/com/leetcode/simple/MergeTwoList.java
@@ -0,0 +1,48 @@
+package com.leetcode.simple;
+
+import org.junit.Test;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ */
+public class MergeTwoList {
+    @Test
+    public void test() {
+        System.out.println(mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(3, null))), new ListNode(2, null)));
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
