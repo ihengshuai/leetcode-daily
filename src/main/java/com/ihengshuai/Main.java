fbbb3a0892f9d708fa743888e8da84d2c08f7d79
ihengshuai <wsm_1105@163.com>
2024-10-11 18:31:43 +0800

    init: 初始化仓库(#0)

---
+++ src/main/java/com/ihengshuai/Main.java
@@ -0,0 +1,14 @@
+package com.ihengshuai;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("test...");
+    }
+}
